a2c439f2fac0eacfc8e6ea154726d4b3599ee6d1
Peter Zuo <peter.zuo@shopee.com>
2021-06-25 10:49:44 +0800

    update ustring: add GetRandomString

--- pkg/ustring.go
+++ pkg/ustring.go
@@ -1,8 +1,23 @@
 package pkg
 
-import "strings"
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
 
 func BreakStringByLine(str string) []string {
 	return strings.Split(str, "\n")
 }
 
+func GetRandomString(length int) string {
+	rand.Seed(time.Now().UnixNano())
+
+	var letterRunes = []rune("0123456789abcdefghijklmnopqrstuvwxyz")
+
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
